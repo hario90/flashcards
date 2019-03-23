0970deab1f8c0ad13fd1fd53d25e4b0f00a3fa3a
lisajoy <lisaharrylock@gmail.com>
2019-03-23 15:36:31 -0700

    use async await for login logic

--- src/renderer/state/user/logics.ts
+++ src/renderer/state/user/logics.ts
@@ -3,7 +3,7 @@ import { AnyAction } from "redux";
 import { createLogic } from "redux-logic";
 
 import { clearDeck, clearDraft, setDecks } from "../deck/actions";
-import { CardResponse, RawDeck } from "../deck/types";
+import { CardResponse, Deck, RawDeck } from "../deck/types";
 import { addRequestToInProgress, removeRequestFromInProgress, setAlert } from "../feedback/actions";
 import { AlertType, HttpRequestType } from "../feedback/types";
 import { setPage } from "../page/actions";
@@ -18,67 +18,54 @@ import { getUserId } from "./selectors";
 import { User } from "./types";
 
 const loginLogic = createLogic({
-    process: ({getState, action, httpClient, baseApiUrl}: ReduxLogicDeps,
-              next: ReduxLogicNextCb, done: ReduxLogicDoneCb) => {
+    process: async ({getState, action, httpClient, baseApiUrl}: ReduxLogicDeps,
+                    next: ReduxLogicNextCb, done: ReduxLogicDoneCb) => {
         const loginAction = action.payload.find((a: AnyAction) => a.type === LOGIN);
         if (!loginAction) {
             done();
         }
 
         const actions: AnyAction[] = [];
-        httpClient.post(`${baseApiUrl}/users/login`, loginAction.payload)
-            .then(({ data }: AxiosResponse<User>) => {
-                actions.push(setUser(data));
-                Promise.all([
-                    httpClient.get(`${baseApiUrl}/decks/users/${data.id}`),
-                    httpClient.get(`${baseApiUrl}/cards/users/${data.id}`),
-                ]).then(([{ data: decks }, { data: cards }]) => {
-                    const decksWithCards = decks.map((d: RawDeck) => {
-                        return {
-                            ...d,
-                            cards: cards.filter((c: CardResponse) => c.deckId === d.id),
-                        };
-                    });
-                    actions.push(
-                        setDecks(decksWithCards),
-                        removeRequestFromInProgress(HttpRequestType.LOGIN),
-                        setPage(Page.Home)
-                        );
-                    next(batchActions(actions));
-                    done();
-                }).catch((err: AxiosError) => {
-                    actions.push(removeRequestFromInProgress(HttpRequestType.LOGIN));
-                    if (err.response && err.response.status === 404) {
-                        actions.push(
-                            setPage(Page.Home)
-                        );
-                    } else if (err.response && err.response.data) {
-                        console.log("err", err.response.data);
-                        actions.push(setAlert({
-                            message: err.response.data,
-                            type: AlertType.ERROR,
-                        }));
-                    }
-                    next(batchActions(actions));
-                    done();
-                });
-            })
-            .catch((err: AxiosError) => {
-                actions.push(removeRequestFromInProgress(HttpRequestType.LOGIN));
-                let message = "Unknown Error";
-                if (err.response && err.response.data) {
-                    message = err.response.data;
-                } else if (err.message) {
-                    message = err.message;
-                }
 
-                actions.push(setAlert({
-                    message,
-                    type: AlertType.ERROR,
-                }));
-                next(batchActions(actions));
-                done();
+        try {
+            const loginUrl = `${baseApiUrl}/users/login`;
+            const { data }: AxiosResponse<User> = await httpClient.post(loginUrl, loginAction.payload);
+            actions.push(setUser(data));
+            const response: [{data: Deck[]}, {data: CardResponse[]}] = await Promise.all([
+                httpClient.get(`${baseApiUrl}/decks/users/${data.id}`),
+                httpClient.get(`${baseApiUrl}/cards/users/${data.id}`),
+            ]);
+            const { data: decks } = response[0];
+            const { data: cards } = response[1];
+            const decksWithCards = decks.map((d: RawDeck) => {
+                return {
+                    ...d,
+                    cards: cards.filter((c: CardResponse) => c.deckId === d.id),
+                };
             });
+            actions.push(
+                setDecks(decksWithCards),
+                removeRequestFromInProgress(HttpRequestType.LOGIN),
+                setPage(Page.Home)
+            );
+            next(batchActions(actions));
+            done();
+        } catch (err) {
+            actions.push(removeRequestFromInProgress(HttpRequestType.LOGIN));
+            let message = "Unknown Error";
+            if (err.response && err.response.data) {
+                message = err.response.data;
+            } else if (err.message) {
+                message = err.message;
+            }
+
+            actions.push(setAlert({
+                message,
+                type: AlertType.ERROR,
+            }));
+            next(batchActions(actions));
+            done();
+        }
     },
     transform: ({getState, action, httpClient, baseApiUrl}: ReduxLogicDeps, next: ReduxLogicNextCb) => {
         next(batchActions([
