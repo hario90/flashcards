332f74df977a3dd43fa6e7cba8d98b04d231ee96
lisajoy <lisaharrylock@gmail.com>
2019-03-23 15:36:11 -0700

    fix types

--- src/renderer/@types/kuroshiro/index.d.ts
+++ src/renderer/@types/kuroshiro/index.d.ts
@@ -1,23 +1,21 @@
 declare module "kuroshiro" {
     class Kuroshiro {
-        public static Util: KuroshiroUtil;
+        public static Util: {
+            IsHiragana(input: string): boolean;
+            IsKatakana(input: string): boolean;
+            isKana(input: string): boolean;
+            isKanji(input: string): boolean;
+            isJapanese(input: string): boolean;
+            hasHiragana(input: string): boolean;
+            hasKatakana(input: string): boolean;
+            hasKana(input: string): boolean;
+            hasKanji(input: string): boolean;
+            kanaToHiragna(input: string): string;
+            kanaToKatakana(input: string): string;
+            kanaToRomaji(input: string): string;
+        };
         constructor();
         public convert(thing: string): Promise<string>;
-    }
-    // tslint:disable-next-line
-    class KuroshiroUtil {
-        public static IsHiragana(input: string): boolean;
-        public static IsKatakana(input: string): boolean;
-        public static isKana(input: string): boolean;
-        public static isKanji(input: string): boolean;
-        public static isJapanese(input: string): boolean;
-        public static hasHiragana(input: string): boolean;
-        public static hasKatakana(input: string): boolean;
-        public static hasKana(input: string): boolean;
-        public static hasKanji(input: string): boolean;
-        public static kanaToHiragna(input: string): string;
-        public static kanaToKatakana(input: string): string;
-        public static kanaToRomaji(input: string): string;
     }
     export = Kuroshiro;
 }
