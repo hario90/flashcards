b4a83e48ad13972ece4e283a49d3fcdfe92a54e3
hario90 <lisaharrylock@gmail.com>
2019-05-19 14:58:39 -0700

    update name of api url env variable

--- src/renderer/constants/index.ts
+++ src/renderer/constants/index.ts
@@ -1,2 +1,2 @@
 export const APP_ID = "app";
-export const BASE_API_URL = process.env.API_URL || "http://localhost:3000";
+export const BASE_API_URL = process.env.ELECTRON_WEBPACK_APP_API_URL || "http://localhost:3000";
